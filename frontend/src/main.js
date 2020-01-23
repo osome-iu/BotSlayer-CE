db94e78fed7de62344f7453496cbb793a835d570
Marc McCarty <mem11@iu.edu>
2020-01-23 11:01:53 -0500

    Should fix npm bug in docker build

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 
 import axios from "axios";
 import VueAxios from "vue-axios";
@@ -64,6 +63,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
